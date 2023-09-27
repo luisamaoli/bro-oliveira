a636f8add52cd24f15f07eb6b5154de138f59ae9
luisamaoli <luisa.maoli@gmail.com>
2023-09-27 19:28:31 -0300

    feat (Tailwind) adding CSS framework

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -4,13 +4,13 @@ import './style.css';
 const NavBar = () => {
 
   return(
-    <nav className='navbar'>
+    <nav className='w-full flex justify-between items-center p-5 bg-white shadow-xl'>
       <span>Bright Routes Organizer</span>
-      <ul style={{ display: 'flex' }}>
-        <li><button>Entrar</button></li>
-        <li><a href=''>BRO Pro</a></li>
-        <li><a href=''>Como Funciona</a></li>
-        <li><a href=''>Quem somos</a></li>
+      <ul className='flex'>
+        <li className='mx-2'><button>Entrar</button></li>
+        <li className='mx-2'><a href=''>BRO Pro</a></li>
+        <li className='mx-2'><a href=''>Como Funciona</a></li>
+        <li className='mx-2'><a href=''>Quem somos</a></li>
       </ul>
     </nav>
   )
