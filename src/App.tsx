c3a7a04daced73aeb46b5285d98da62451216b71
luisamaoli <luisa.maoli@gmail.com>
2023-10-09 19:05:14 -0300

    feat (Promisse, assincronia & Map class)

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import { Hero } from './components/Hero';
 import { NavBar } from './components/NavBar';
 import { ItemListContainer } from './components/ItemListContainer';
 import './App.css';
+import { Card } from './components/Card';
 
 function App() {
   return (
     <div className="App">
       <NavBar nameCompany='Fairies Wear Boots' />
+      <Card />
       <ItemListContainer name='Escolha as suas favoritas'/>
       <Hero />
     </div>
--- src/components/ItemListContainer/index.tsx
+++ src/components/ItemListContainer/index.tsx
@@ -1,5 +1,4 @@
 import { BuyerButton } from "../BuyerButton";
-import { Card } from "../Card";
 import { Counter } from "../Counter";
 
 // interface funciona como um contracts
@@ -13,17 +12,17 @@ const ItemListContainer = ({ name }: IProps) => {
       <h1 className="text-3xl font-bold mb-4">{name}</h1>
       <div className="flex items-center space-x-4">
         <div className="flex flex-col items-center">
-          <Card name= {require("../../img/bordada.png")}/>
+          <img className='w-1/2 h-auto max-w-full' src={require("../../img/bordada.png")} alt="" />
           <Counter stock={10} initial={1}/>
         </div>
 
         <div className="flex flex-col items-center">
-          <Card name= {require("../../img/snakeboots.png")}/>
+          <img className='w-1/2 h-auto max-w-full' src={require("../../img/snakeboots.png")} alt="" />
           <Counter stock={5} initial={0}/>
         </div>
 
         <div className="flex flex-col items-center">
-          <Card name= {require("../../img/croco.png")}/>
+          <img className='w-1/2 h-auto max-w-full' src={require("../../img/croco.png")} alt="" />
           <Counter stock={12} initial={2}/>
         </div>
       </div>
